01d3da97bb8a8c54776016883997a4b833401522
Jirawat I <nodtem66@gmail.com>
2019-01-31 23:09:23 +0700

    add os_on_assert_callback()

--- src/os_assert.c
+++ src/os_assert.c
@@ -37,11 +37,18 @@
 
 #include "cocoos.h"
 
-void os_on_assert( uint16_t line ) {
+static void (*user_callback)(const char*, uint16_t, const char*);
+
+void os_on_assert(const char* file, uint16_t line, const char* expr) {
     static volatile uint16_t l;
     os_disable_interrupts();
     l = line;
     l = l;
+    user_callback(file, line, expr);
     while(1);
+}
+
+void os_on_assert_attach_callback(void (*callback)(const char*, uint16_t, const char*)) {
+    user_callback = callback;
 }
 
