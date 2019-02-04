a2a11a8f335e98e109ff975ea193b4de08b11e10
Jirawat I <nodtem66@gmail.com>
2019-02-05 04:05:55 +0700

    PR: new idea to utilize `os_on_assert` (#11)
    * add os_on_assert_callback()
    
    * Update os_assert.c
    
    * Update os_assert.c

--- inc/os_assert.h
+++ inc/os_assert.h
@@ -44,12 +44,11 @@
 extern "C" {
 #endif
 
-void os_on_assert( uint16_t line );
+void os_on_assert( const char* filename, uint16_t line, const char* expression);
+void os_on_assert_attach_callback( void (*callback)(const char*, uint16_t, const char*) );
 
 #ifndef NASSERT
-#define os_assert( test )   if ( !(test) ) {\
-                                os_on_assert(__LINE__);\
-                            }
+#define os_assert( test )   if ( !(test) ) {os_on_assert(__FILE__, __LINE__, #test);}
 #else
 #define os_assert( test )
 #endif
--- src/os_assert.c
+++ src/os_assert.c
@@ -37,11 +37,22 @@
 
 #include "cocoos.h"
 
-void os_on_assert( uint16_t line ) {
+static void (*user_callback)(const char*, uint16_t, const char*) = (void*)0;
+
+void os_on_assert(const char* file, uint16_t line, const char* expr) {
     static volatile uint16_t l;
     os_disable_interrupts();
     l = line;
     l = l;
+ 
+    if (0 != user_callback) {
+      user_callback(file, line, expr);
+    }
+ 
     while(1);
+}
+
+void os_on_assert_attach_callback(void (*callback)(const char*, uint16_t, const char*)) {
+    user_callback = callback;
 }
 
