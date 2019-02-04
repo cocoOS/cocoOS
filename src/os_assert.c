ebe60faf5ffa47c4d89abe99edaefabb4916330a
cocoOS <42898685+cocoOS@users.noreply.github.com>
2019-02-04 22:02:48 +0100

    Update os_assert.c

--- src/os_assert.c
+++ src/os_assert.c
@@ -37,14 +37,18 @@
 
 #include "cocoos.h"
 
-static void (*user_callback)(const char*, uint16_t, const char*);
+static void (*user_callback)(const char*, uint16_t, const char*) = (void*)0;
 
 void os_on_assert(const char* file, uint16_t line, const char* expr) {
     static volatile uint16_t l;
     os_disable_interrupts();
     l = line;
     l = l;
-    user_callback(file, line, expr);
+ 
+    if ( 0 != user_callback) {
+      user_callback(file, line, expr);
+    }
+ 
     while(1);
 }
 
